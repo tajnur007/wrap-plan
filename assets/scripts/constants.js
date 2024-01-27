7b0065103949749c6571462f364acfb92fd70447
Kazi Tajnur Islam <tajnur007@gmail.com>
2024-01-27 23:55:53 +0600

    DB initialization, data load from JSON file and necessary DOM node access point done

---
+++ assets/scripts/constants.js
@@ -0,0 +1,13 @@
+export const DB_KEYS = {
+  NEW: 'new',
+  CLAIMS: 'claims',
+  DRAFT: 'draft',
+  COMPLETE: 'completed',
+};
+
+export const PROJECT_STATUS = {
+  NEW: 'New Project',
+  CLAIMS: 'Claims Generated',
+  DRAFT: 'Draft Generated',
+  COMPLETE: 'Completed',
+};
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('Hello World!');
+import jsonData from '../../resources/data.json' assert { type: 'json' };
+import timeData from '../../resources/time.json' assert { type: 'json' };
+import { DB_KEYS, PROJECT_STATUS } from './constants.js';
+
+const newProjectContainerNode = document.getElementById('newProjectCardsContainer');
+const claimsGeneratedContainerNode = document.getElementById('claimsGeneratedCardsContainer');
+const draftGeneratedContainerNode = document.getElementById('draftGeneratedCardsContainer');
+const markedCompleteContainerNode = document.getElementById('markedCompleteCardsContainer');
+const startNewProjectNode = document.getElementById('addNewProjectCard');
+
+const DB = new Map();
+
+const initializeDb = () => {
+  Object.values(DB_KEYS).map(key => {
+    DB.set(key, []);
+  });
+};
+
+const loadDataFromJsonFileToDb = () => {
+  const newProjects = [];
+  const claimProjects = [];
+  const draftProjects = [];
+  const completeProjects = [];
+
+  jsonData.map(project => {
+    if (project.status === PROJECT_STATUS.NEW) {
+      newProjects.push(project);
+    } else if (project.status === PROJECT_STATUS.CLAIMS) {
+      claimProjects.push(project);
+    } else if (project.status === PROJECT_STATUS.DRAFT) {
+      draftProjects.push(project);
+    } else if (project.status === PROJECT_STATUS.COMPLETE) {
+      completeProjects.push(project);
+    }
+  });
+
+  DB.set(DB_KEYS.NEW, newProjects);
+  DB.set(DB_KEYS.CLAIMS, claimProjects);
+  DB.set(DB_KEYS.DRAFT, draftProjects);
+  DB.set(DB_KEYS.COMPLETE, completeProjects);
+};
+
+initializeDb();
+loadDataFromJsonFileToDb();
+console.log('DB::', DB);
