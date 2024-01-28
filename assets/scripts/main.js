bda1f091f269a27f379056a2552363e263ea8441
Kazi Tajnur Islam <tajnur007@gmail.com>
2024-01-29 00:34:32 +0600

    Action dropdown design done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,6 +19,10 @@ const claimsGeneratedData = [];
 const draftGeneratedData = [];
 const markedCompleteData = [];
 
+let numberOfTotalProjects = 0;
+let currentClickedProjectId = null;
+let currentClickedProjectStatus = null;
+
 const loadDataFromJsonFileToStore = () => {
   jsonData.forEach((project, id) => {
     const projectWithId = { id, ...project };
@@ -33,6 +37,8 @@ const loadDataFromJsonFileToStore = () => {
       markedCompleteData.push(projectWithId);
     }
   });
+
+  numberOfTotalProjects = numberOfTotalProjects.length;
 };
 
 const loadDataFromStoreToDom = () => {
@@ -114,4 +120,38 @@ window.closeCreateProjectModal = () => {
 
 window.showActionBox = (projectId, projectStatus) => {
   console.log('Project:::', projectId, projectStatus);
+  currentClickedProjectId = projectId;
+  currentClickedProjectStatus = projectStatus;
+};
+
+window.updateProjectStatus = (targetedStatus) => {
+  let targetedProject;
+
+  if (currentClickedProjectStatus === PROJECT_STATUS.NEW) {
+    targetedProject = newProjectsData.find(project => project.id === currentClickedProjectId);
+    newProjectsData = newProjectsData.filter(project => project.id !== currentClickedProjectId);
+  } else if (currentClickedProjectStatus === PROJECT_STATUS.CLAIMS) {
+    targetedProject = claimsGeneratedData.find(project => project.id === currentClickedProjectId);
+    claimsGeneratedData = claimsGeneratedData.filter(project => project.id !== currentClickedProjectId);
+  } else if (currentClickedProjectStatus === PROJECT_STATUS.DRAFT) {
+    targetedProject = draftGeneratedData.find(project => project.id === currentClickedProjectId);
+    draftGeneratedData = draftGeneratedData.filter(project => project.id !== currentClickedProjectId);
+  } else if (currentClickedProjectStatus === PROJECT_STATUS.COMPLETE) {
+    targetedProject = markedCompleteData.find(project => project.id === currentClickedProjectId);
+    markedCompleteData = markedCompleteData.filter(project => project.id !== currentClickedProjectId);
+  }
+
+  targetedProject.status = targetedStatus;
+
+  if (targetedStatus === PROJECT_STATUS.NEW) {
+    newProjectsData.push(targetedProject);
+  } else if (currentClickedProjectStatus === PROJECT_STATUS.CLAIMS) {
+    claimsGeneratedData.push(targetedProject);
+  } else if (currentClickedProjectStatus === PROJECT_STATUS.DRAFT) {
+    draftGeneratedData.push(targetedProject);
+  } else if (currentClickedProjectStatus === PROJECT_STATUS.COMPLETE) {
+    markedCompleteData.push(targetedProject);
+  }
+
+  loadDataFromStoreToDom();
 };
