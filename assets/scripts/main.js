562b4a1bc8c3f8a01b842d72cb9e00abdeb2f1c5
Kazi Tajnur Islam <tajnur007@gmail.com>
2024-01-28 14:17:09 +0600

    Update counts for actual data

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,24 +6,39 @@ const newProjectContainerNode = document.getElementById('newProjectCardsContaine
 const claimsGeneratedContainerNode = document.getElementById('claimsGeneratedCardsContainer');
 const draftGeneratedContainerNode = document.getElementById('draftGeneratedCardsContainer');
 const markedCompleteContainerNode = document.getElementById('markedCompleteCardsContainer');
+
+const newProjectCountNode = document.getElementById('newProjectCount');
+const claimsGeneratedCountNode = document.getElementById('claimsGeneratedCount');
+const draftGeneratedCountNode = document.getElementById('draftGeneratedCount');
+const markedCompleteCountNode = document.getElementById('markedCompleteCount');
+
 const startNewProjectNode = document.getElementById('addNewProjectCard');
 const createProjectModalNode = document.getElementById('createProjectModal');
 const modalCloseBtnNode = document.getElementById('modalCloseBtn');
+
+let numOfNewProjects = 0;
+let numOfClaimsGenerated = 0;
+let numOfDraftGenerated = 0;
+let numOfMarkedComplete = 0;
 
 const loadDataFromJsonFileToDom = () => {
   jsonData.map(project => {
     if (project.status === PROJECT_STATUS.NEW) {
       const card = generateCard(project, BORDER_CLASSES.NEW);
       newProjectContainerNode.appendChild(card);
+      numOfNewProjects++;
     } else if (project.status === PROJECT_STATUS.CLAIMS) {
       const card = generateCard(project, BORDER_CLASSES.CLAIMS);
       claimsGeneratedContainerNode.appendChild(card);
+      numOfClaimsGenerated++;
     } else if (project.status === PROJECT_STATUS.DRAFT) {
       const card = generateCard(project, BORDER_CLASSES.DRAFT);
       draftGeneratedContainerNode.appendChild(card);
+      numOfDraftGenerated++;
     } else if (project.status === PROJECT_STATUS.COMPLETE) {
       const card = generateCard(project, BORDER_CLASSES.COMPLETE);
       markedCompleteContainerNode.appendChild(card);
+      numOfMarkedComplete++;
     }
   });
 };
@@ -57,7 +72,15 @@ const generateCard = (project, borderClass) => {
   return card;
 };
 
+const updateCounts = () => {
+  newProjectCountNode.innerText = numOfNewProjects;
+  claimsGeneratedCountNode.innerText = numOfClaimsGenerated;
+  draftGeneratedCountNode.innerText = numOfDraftGenerated;
+  markedCompleteCountNode.innerText = numOfMarkedComplete;
+};
+
 loadDataFromJsonFileToDom();
+updateCounts();
 
 const openCreateProjectModal = () => {
   createProjectModalNode.style.display = 'flex';
