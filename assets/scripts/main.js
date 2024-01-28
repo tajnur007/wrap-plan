38334c1cba2cbe4213c5a7fa31a805486f6c8867
Kazi Tajnur Islam <tajnur007@gmail.com>
2024-01-29 01:49:27 +0600

    Action dropdown correct position appearence and project swapping handled

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,13 +13,15 @@ const draftGeneratedCountNode = document.getElementById('draftGeneratedCount');
 const markedCompleteCountNode = document.getElementById('markedCompleteCount');
 
 const createProjectModalNode = document.getElementById('createProjectModal');
+const actionDropdownNode = document.getElementById('actionDropdown');
 
-const newProjectsData = [];
-const claimsGeneratedData = [];
-const draftGeneratedData = [];
-const markedCompleteData = [];
+let newProjectsData = [];
+let claimsGeneratedData = [];
+let draftGeneratedData = [];
+let markedCompleteData = [];
 
 let numberOfTotalProjects = 0;
+let currentClickedActionBtnId = null;
 let currentClickedProjectId = null;
 let currentClickedProjectStatus = null;
 
@@ -42,25 +44,31 @@ const loadDataFromJsonFileToStore = () => {
 };
 
 const loadDataFromStoreToDom = () => {
+  newProjectContainerNode.innerHTML = '';
   newProjectsData.forEach(project => {
     const card = generateCard(project, BORDER_CLASSES.NEW);
     newProjectContainerNode.appendChild(card);
   });
 
+  claimsGeneratedContainerNode.innerHTML = '';
   claimsGeneratedData.forEach(project => {
     const card = generateCard(project, BORDER_CLASSES.CLAIMS);
     claimsGeneratedContainerNode.appendChild(card);
   });
 
+  draftGeneratedContainerNode.innerHTML = '';
   draftGeneratedData.forEach(project => {
     const card = generateCard(project, BORDER_CLASSES.DRAFT);
     draftGeneratedContainerNode.appendChild(card);
   });
 
+  markedCompleteContainerNode.innerHTML = '';
   markedCompleteData.forEach(project => {
     const card = generateCard(project, BORDER_CLASSES.COMPLETE);
     markedCompleteContainerNode.appendChild(card);
   });
+
+  updateCounts();
 };
 
 const generateCard = (project, borderClass) => {
@@ -70,8 +78,8 @@ const generateCard = (project, borderClass) => {
   card.innerHTML = `
     <div class="card h-full d-flex flex-column justify-between">
       <h3 class="card-title">${project.name}</h3>
-      <div class="three-dot center-y h-full" onclick="showActionBox('${project.id}, ${project.status}')">
-        <div class="d-flex flex-column px-1">
+      <div class="three-dot center-y h-full">
+        <div id="${'threeDot' + project.id}" class="d-flex flex-column px-1" onclick="showActionBox('${'threeDot' + project.id}', '${project.id}', '${project.status}')">
           <div class="dot"></div>
           <div class="dot"></div>
           <div class="dot"></div>
@@ -108,7 +116,6 @@ const updateCounts = () => {
 
 loadDataFromJsonFileToStore();
 loadDataFromStoreToDom();
-updateCounts();
 
 window.openCreateProjectModal = () => {
   createProjectModalNode.style.display = 'flex';
@@ -118,40 +125,54 @@ window.closeCreateProjectModal = () => {
   createProjectModalNode.style.display = 'none';
 };
 
-window.showActionBox = (projectId, projectStatus) => {
-  console.log('Project:::', projectId, projectStatus);
-  currentClickedProjectId = projectId;
-  currentClickedProjectStatus = projectStatus;
+window.showActionBox = (actionBtnId, projectId, projectStatus) => {
+  if (currentClickedActionBtnId === actionBtnId) {
+    currentClickedActionBtnId = null;
+    actionDropdownNode.style.display = 'none';
+  } else {
+    const clickedActionBtnNode = document.getElementById(actionBtnId);
+    const actionBtnPosition = clickedActionBtnNode.getBoundingClientRect();
+
+    currentClickedActionBtnId = actionBtnId;
+
+    actionDropdownNode.style.display = 'block';
+    actionDropdownNode.style.top = `${actionBtnPosition.y - 45}px`;
+    actionDropdownNode.style.left = `${actionBtnPosition.x - 120}px`;
+
+    currentClickedProjectId = projectId;
+    currentClickedProjectStatus = projectStatus;
+  }
 };
 
 window.updateProjectStatus = (targetedStatus) => {
   let targetedProject;
 
   if (currentClickedProjectStatus === PROJECT_STATUS.NEW) {
-    targetedProject = newProjectsData.find(project => project.id === currentClickedProjectId);
-    newProjectsData = newProjectsData.filter(project => project.id !== currentClickedProjectId);
+    targetedProject = newProjectsData.find(project => project.id == currentClickedProjectId);
+    newProjectsData = newProjectsData.filter(project => project.id != currentClickedProjectId);
   } else if (currentClickedProjectStatus === PROJECT_STATUS.CLAIMS) {
-    targetedProject = claimsGeneratedData.find(project => project.id === currentClickedProjectId);
-    claimsGeneratedData = claimsGeneratedData.filter(project => project.id !== currentClickedProjectId);
+    targetedProject = claimsGeneratedData.find(project => project.id == currentClickedProjectId);
+    claimsGeneratedData = claimsGeneratedData.filter(project => project.id != currentClickedProjectId);
   } else if (currentClickedProjectStatus === PROJECT_STATUS.DRAFT) {
-    targetedProject = draftGeneratedData.find(project => project.id === currentClickedProjectId);
-    draftGeneratedData = draftGeneratedData.filter(project => project.id !== currentClickedProjectId);
+    targetedProject = draftGeneratedData.find(project => project.id == currentClickedProjectId);
+    draftGeneratedData = draftGeneratedData.filter(project => project.id != currentClickedProjectId);
   } else if (currentClickedProjectStatus === PROJECT_STATUS.COMPLETE) {
-    targetedProject = markedCompleteData.find(project => project.id === currentClickedProjectId);
-    markedCompleteData = markedCompleteData.filter(project => project.id !== currentClickedProjectId);
+    targetedProject = markedCompleteData.find(project => project.id == currentClickedProjectId);
+    markedCompleteData = markedCompleteData.filter(project => project.id != currentClickedProjectId);
   }
 
   targetedProject.status = targetedStatus;
 
   if (targetedStatus === PROJECT_STATUS.NEW) {
     newProjectsData.push(targetedProject);
-  } else if (currentClickedProjectStatus === PROJECT_STATUS.CLAIMS) {
+  } else if (targetedStatus === PROJECT_STATUS.CLAIMS) {
     claimsGeneratedData.push(targetedProject);
-  } else if (currentClickedProjectStatus === PROJECT_STATUS.DRAFT) {
+  } else if (targetedStatus === PROJECT_STATUS.DRAFT) {
     draftGeneratedData.push(targetedProject);
-  } else if (currentClickedProjectStatus === PROJECT_STATUS.COMPLETE) {
+  } else if (targetedStatus === PROJECT_STATUS.COMPLETE) {
     markedCompleteData.push(targetedProject);
   }
 
+  actionDropdownNode.style.display = 'none';
   loadDataFromStoreToDom();
 };
